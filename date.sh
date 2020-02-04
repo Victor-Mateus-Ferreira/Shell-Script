638c9e7384d83fec397374afb6e37123733d1372
Victor Mateus <victormateuslf@gmail.com>
2020-02-04 15:24:14 -0300

    Shell Script

---
+++ date.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+datadehoje=`date`
+
+echo $datadehoje
+
---
+++ operadores.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read -p "Digite um Numero: " num1
+
+read -p "Digite o segundo Numero: " num2
+
+echo $[num1 + num2]
+
